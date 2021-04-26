6c8316325a2db6e778841896fdeee4515f7bead4
pablo <uriartepablo23@gmail.com>
2021-04-26 08:46:02 -0400

    pantallanueva
    (cherry picked from commit d5611d417362701b9c5f42203e64a44ce9a37da0)

---
+++ app/src/main/java/com/example/hello_world/OptionsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hello_world
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OptionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.options_main)
+    }
+}
