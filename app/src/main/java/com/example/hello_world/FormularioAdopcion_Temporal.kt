1b0e8facc041bbfd4de6ab9e6e80e9e2f0d39333
HP 15-AQ173CL <daniela13.37za@gmail.com>
2021-05-11 08:11:25 -0400

    DanielaFormulario

---
+++ app/src/main/java/com/example/hello_world/FormularioAdopcion_Temporal.kt
@@ -0,0 +1,8 @@
+package com.example.hello_world
+
+import com.example.hello_world.FormularioAdopcionTemporal.AddAdopcionActivity
+
+object FormularioAdopcionTemporal {
+    val  AddAdopcionActivity = mutableListOf<FormularioAdopcionActivity.FormularioAdopcion>()
+
+}
---
+++ app/src/main/java/com/example/hello_world/RegistroPerdida_Temporal.kt
@@ -0,0 +1,5 @@
+package com.example.hello_world
+
+object RegistroPerdidaTemporal {
+    val  AddPerdidaActivity = mutableListOf< AddPerdidaActivity.RegistroPerdida>( )
+}
